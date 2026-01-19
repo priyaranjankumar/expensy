b80afbccbc54e8815a5244fa9f6143bae858f0f3
Priyaranjan.kumar <priyaranjankumar.work@gmail.com>
2026-01-20 00:54:57 +0530

    Add entry point for PyInstaller

---
+++ backend/run.py
@@ -0,0 +1,7 @@
+import uvicorn
+import multiprocessing
+from app.main import app
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
